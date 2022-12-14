2b35c3b9b427929df0b89a646b64f646344b1174
itohansan <itohanidubor@gmail.com>
2022-12-14 15:16:33 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 	int count;
 	char letter;
 
-	while (count <= 9)
+	while (count = 10)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('/n');
+		_putchar('\n');
 	}
 }
