5c59604347460e0b2c98ec6138b46029925fc307
itohansan <itohanidubor@gmail.com>
2022-12-14 19:06:57 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,18 +16,24 @@ void times_table(void)
 			c = a * b;
 			if ((c / 10) == 0)
 			{
-			if (b == 0)
+				if (b == 0)
 				{
-				_putchar('0');
-			}
-			if (b != 0)
-				_putchar(' ');
-				_putchar((c % 10) + '0');
-			}
-			if (b < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+			
+					_putchar('0');
+		
+				}
+		
+				if (b != 0)
+				{
+			
+					_putchar(' ')
+					_putchar((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 	}
