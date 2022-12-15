9a6452584309f5c20c4c385112249a45ac2623af
itohansan <itohanidubor@gmail.com>
2022-12-15 08:45:30 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test functions to see if integer is positive or negative
+ * positive_or_negative - negative or positive integer test
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
