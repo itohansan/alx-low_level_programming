0345b776e592069b1e7d5be101d9f742fa4a9f07
itohansan <itohanidubor@gmail.com>
2022-12-14 19:19:05 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 				if (b != 0)
 				{
 			
-					_putchar(' ')
+					_putchar(' ');
 					_putchar((c % 10) + '0');
 				}
 				if (b < 9)
@@ -35,6 +35,18 @@ void times_table(void)
 					_putchar(' ');
 				}
 			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
+		_putchar ('\n');
 	}
+	
 }
