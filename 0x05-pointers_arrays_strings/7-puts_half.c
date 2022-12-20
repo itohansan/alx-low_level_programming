479c336b7a36f41881a7d0a57f791267f23420d6
itohansan <itohanidubor@gmail.com>
2022-12-19 18:49:03 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,20 @@
 void puts_half(char *str)
 {
 	int index, half;
+
 	index = 0;
 	while (str[index] != '\0')
 		index++;
+
 	half = index / 2;
+
 	if (index % 2 == 1)
-		half ++;
+		half++;
+
 	while (half < index)
 	{
 		_putchar(str[half]);
-		half++
+		half++;
 	}
 	_putchar('\n');
 }
