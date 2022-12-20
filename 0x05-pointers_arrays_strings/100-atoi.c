c82d0f71d14e6b1c06c33ed2840fa8ced95e04c9
itohansan <itohanidubor@gmail.com>
2022-12-19 19:27:47 -0800

    "advance task"

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _atoi - convert string to integer
+ * @s: Pointer to a string
+ *
+ * Return: void;
+ */
+
+int _atoi(char *s)
+{
+	int m;
+	unsigned int num;
+	char *temp;
+
+	temp = s;
+	num = 0;
+	m = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			m *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (temp - '0');
+			temp++
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * m);
