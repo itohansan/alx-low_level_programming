dcea2496d889a56daa881a27f6e2ef3e3e9f2a67
itohansan <itohanidubor@gmail.com>
2022-12-23 17:09:47 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,7 @@
 #define MAIN_H
 #include <stdio.h>
+#ifndef MAIN_H
 
-/**
- * void prototype()
- * int prototype()
- * char prototype()
- */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
